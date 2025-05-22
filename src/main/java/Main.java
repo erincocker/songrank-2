aa3d2c6363e293aa17462b5e06cd852cff48c031
erincocker <erin618c@gmail.com>
2025-05-22 16:17:07 +0100

    Changed from pojos to jsonnode, resolves issue #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        try {
+//            JsonNode jsonNode = objectMapper.readTree(new File("src/main/resources/testPlaylist.json"));
+//            System.out.println(jsonNode.get("items").get(0).get("name"));
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
